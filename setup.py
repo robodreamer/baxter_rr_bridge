9934c94d8654abfd95c6a46298ae30d4444ea5bc
Andy Park <andypark@purdue.edu>
2015-02-28 12:01:45 -0500

    modified setup

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['baxter_rr_bridge', 'baxter_external_devices']
-d['package_dir'] = {'': '/home/artlab2/ros_ws/src'}
+#d['packages'] = ['baxter_rr_bridge', 'baxter_external_devices']
+#d['package_dir'] = {'': 'src'}
+#d['package_dir'] = {'': 'src'}
+#d['packages'] = ['baxter_rr_bridge']
+#d['package_dir'] = {'': '/home/artlab2/ros_ws/src'}
+
 
 setup(**d)
